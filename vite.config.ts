07fe417911f206b8fd8676489a0a85ac1c1e3131
Lio Mikael <lmybanez@ndkc.edu.ph>
2026-08-10 09:47:56 +0800

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/https://github.com/lmybanez-web/Personal-Portfolio/', // ⚠️ Must match your repo name exactly!
   plugins: [react()],
-});
+})
